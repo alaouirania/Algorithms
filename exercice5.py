869e774b404bcd396dcca91d7b2554d8e66cc405
alaouirania <75624037+alaouirania@users.noreply.github.com>
2021-04-01 15:00:23 +0100

    Update exercice5.py

--- exercice5.py
+++ exercice5.py
@@ -1,16 +1,28 @@
-memo = dict()
+memo = dict() #on met en place un disctionnaire pour mémoriser les valeurs
 def set(liste,sommeRecherche):
-    resultat = 0
-    if sommeRecherche in memo:
+    liste1 = [] #création de liste où l'on va mettre les valeurs inférieures de la somme recherchée
+    for j in liste:
+        if j < sommeRecherche:
+            liste1.append(j) #à chaque fois que l'on trouve une valeur inférieure on l'ajoute dans la liste vide
+    if sommeRecherche in memo: #on stock les valeurs dans le dictionnaire
         return memo.get(sommeRecherche)
-    if sommeRecherche == 0:
-        return resultat
+    if sommeRecherche == 0:  #si la somme recherchée est == 0 on return Faux
+        return False
     else:
-        for i in liste:
-             while resultat != sommeRecherche:
-                resultat = liste[i] + liste[i+1]
-                return resultat
-
-print(set([3, 34, 4, 12, 5, 2],7))
-
-#on additionne petit à petit jusqu'a trouver la somme voulue
+        for num in liste1:
+            SumIs = num
+            for i in range(0, len(liste1)):
+                for e in range(0, len(liste1)):  #liste imbriquée pour comparer les valeurs
+                     if i != e:
+                         if SumIs + liste1[e] <= sommeRecherche:
+                            SumIs += liste1[e] #on additionne les nombres jusqu'à trouver le résultat voulu
+                         elif SumIs == sommeRecherche:
+                            return True
+                     if SumIs == sommeRecherche:
+                         return True
+        return False
+print(set([34, 12, 3, 5, 2, 3],9))
+'''     
+    Complexité:
+Utilisation de méthode memoisation donc algorithmes dynamiques ==> O(n)
+'''
